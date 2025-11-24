fa0bf72c5108dd705217615e39323a7c880243ed
Somi Hwang <somi.c.hwang@gmail.com>
2025-11-24 16:41:12 +0900

    feat: 공통 컴포넌트 typography 구현

---
+++ src/components/Typography.tsx
@@ -0,0 +1,39 @@
+import styled from "@emotion/native";
+import { theme } from "@/styles/theme";
+import { TextProps, TextStyle, ViewStyle } from "react-native";
+
+export interface TypographyProps {
+  color: keyof typeof theme.colors | string;
+  name: keyof typeof theme.fontSize;
+  text: string;
+  textProps?: TextProps;
+  style?: TextStyle;
+  containerStyle?: ViewStyle;
+  lineHeightPixel?: number;
+}
+
+const Typography = (props: TypographyProps) => {
+  const { color, name, lineHeightPixel, text, textProps = {}, style = {}, containerStyle = {} } = props;
+
+  return (
+    <Container style={containerStyle}>
+      <Text name={name} color={color} lineHeightPixel={lineHeightPixel} {...textProps} style={style}>
+        {text}
+      </Text>
+    </Container>
+  );
+};
+
+const Container = styled.View``;
+
+const Text = styled.Text<{
+  name: keyof typeof theme.fontSize;
+  color: string;
+  lineHeightPixel: number | undefined;
+}>`
+  color: ${({ color }) => color};
+  font-size: ${({ name }) => theme.fontSize[name]};
+  ${({ lineHeightPixel }) => lineHeightPixel && `line-height: ${lineHeightPixel};`}
+`;
+
+export default Typography;
