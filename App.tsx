5dc3bc8811c9831aeba427b25364e88d6f73eae6
Somi Hwang <somi.c.hwang@gmail.com>
2025-11-24 18:47:00 +0900

    feat: 테스트를 위한 홈 스크린 업데이트

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
-import { QueryClientProvider } from '@tanstack/react-query';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { queryClient } from '@/lib/queryClient';
-import { HomeScreen } from '@/screens/HomeScreen';
+import { QueryClientProvider } from "@tanstack/react-query";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { queryClient } from "@/lib/queryClient";
+import UserFormScreen from "@/screens/UserFormScreen";
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <QueryClientProvider client={queryClient}>
-        <HomeScreen />
+        <UserFormScreen />
       </QueryClientProvider>
     </SafeAreaProvider>
   );
